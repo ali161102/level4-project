414c5e35d5a9a876d53e8027d96fd708bf5273a1
2388934m <2388934m@student.gla.ac.uk>
2021-10-23 00:55:10 +0100

    added print course horizontally function

--- other/methods.java
+++ other/methods.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class methods {
     /* class for generating leads and courses based on methods */
 
-    public static void addMethod(String filename, int lead_length) {
+    public static String createMethod(String filename, int lead_length) {
         ArrayList<String> lead = new ArrayList<String>();
         try {
             File rows = new File(filename);
@@ -20,22 +20,25 @@ public class methods {
             System.out.println("no such file found");
       // e.printStackTrace();
         }
-        createArray(lead);
+        String method_array = createArray(lead);
+        return(method_array);
         // print(lead);
     }
 
-    public static void createArray(ArrayList<String> lead) {
+    public static String createArray(ArrayList<String> lead) {
         // int[][] method = new int[lead.get(0).length()][lead.size()];
         ArrayList<ArrayList<Integer>> method = new ArrayList<ArrayList<Integer>>();
-        System.out.println(lead);
+        int numbells = lead.get(0).length();
+        System.out.println("this is the lead" + lead);
+        System.out.println("numbells" + numbells);
         // System.out.println(method);
 
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < numbells; i++) {
             method.add(new ArrayList<Integer>());
         }
 
         for (String row : lead) {
-            for (int i = 0; i < row.length(); i++) {
+            for (int i = 0; i < numbells; i++) {
                 char c = row.toCharArray()[i];
                 int bell = Character.getNumericValue(c);
                 method.get(bell-1).add(i);
@@ -43,8 +46,10 @@ public class methods {
         }
         String method_str = method.toString();
         String replacedBrackets = method_str.replace("[", "{").replace("]", "}");
+        
+        return(replacedBrackets);
 
-        System.out.println(replacedBrackets);
+        // System.out.println(replacedBrackets);
     }
 
     
@@ -105,12 +110,35 @@ public class methods {
         System.out.println("------------");
     }
 
+    public static void printCourseHorizontal(String head) {
+        String lead_end = new String();
+        String lead_head = head;
+        ArrayList<ArrayList<String>> leads = new ArrayList<ArrayList<String>>();
+        while (!lead_end.equals(head)) {
+            
+            ArrayList<String> lead = lead(lead_head);
+            leads.add(lead); 
+            lead_end = lead.get(lead.size()-1);
+            // print(lead);
+            lead_head = lead_end;
+        } 
+
+        for (int i = 0; i < leads.get(0).size()-1; i++) {
+            String output = "";
+            for (int j = 0; j < leads.size(); j++) {
+
+                output += leads.get(j).get(i).replace("", " ").trim().replace("1", " ")+ " | ";
+                //System.out.println("i = " + i + "\tj = " + j);
+            }
+            System.out.println(output);
+        }
+    }
     public static void main(String[] args) {
 
-        //addMethod("LSM_rows.txt", 24);
-
+        String CSM_array = createMethod("cambridge_surprise_major.txt", 32);
+        System.out.println(CSM_array);
         // print(lead("164523"));
-        printCourse("123456");
+        // printCourse("123456");
 
     }
 }
